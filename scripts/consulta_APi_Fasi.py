fd86f615a5ba55448488422b4af457c6d86df372
Henrique C. Rodrigues <henriquehoor18@hotmail.com>
2026-04-07 20:35:33 -0300

    feat:pesrisssrsistência dos dados vindos da api_fasi

--- scripts/consulta_APi_Fasi.py
+++ scripts/consulta_APi_Fasi.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+from src.database.engine import criando_usuario
 import os
 import requests
 import dotenv
-import json
+
 
 dotenv.load_dotenv()
 
@@ -23,5 +25,33 @@ for i in range(1,6):
     except requests.HTTPError as e:
         print(f'Erro no request: {e}')
     else:
-        print(response.status_code)
-
+        response_json = response.json()
+        todos_os_dados = response_json['dados']
+        if todos_os_dados is not None:
+            for registros in todos_os_dados:
+                registros['data_hora'] = datetime.fromisoformat(registros['data_hora'])
+                criando_usuario(
+                    matricula=registros['matricula'],
+                    periodo=registros['periodo'],
+                    genero=registros['genero'],
+                    polo=registros['polo'],
+                    cor_etnia=registros['cor_etnia'],
+                    pcd=registros['pcd'],
+                    tipo_deficiencia=registros['tipo_deficiencia'],
+                    renda=registros['renda'],
+                    deslocamento=registros['deslocamento'],
+                    trabalho=registros['trabalho'],
+                    assistencia_estudantil=registros['assistencia_estudantil'],
+                    saude_mental=registros['saude_mental'],
+                    estresse=registros['estresse'],
+                    acompanhamento=registros['acompanhamento'],
+                    escolaridade_pai=registros['escolaridade_pai'],
+                    escolaridade_mae=registros['escolaridade_mae'],
+                    qtd_computador=registros['qtd_computador'],
+                    qtd_celular=registros['qtd_celular'],
+                    computador_proprio=registros['computador_proprio'],
+                    gasto_internet=registros['gasto_internet'],
+                    acesso_internet=registros['acesso_internet'],
+                    tipo_moradia=registros['tipo_moradia'],
+                    data_hora=registros['data_hora'],
+                        )
